397ee600310a557054ee7e269e15a782f9da6116
zhangxy <zhangxy@github.com>
2017-08-24 11:45:48 +0800

    init momo

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by pengZY on 2017/8/24.
+//  Copyright © 2017年 swl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by pengZY on 2017/8/24.
+//  Copyright © 2017年 swl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
